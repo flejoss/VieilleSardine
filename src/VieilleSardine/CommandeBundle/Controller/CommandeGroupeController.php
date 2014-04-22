666a88a493aa544c53da9f729aca7e591f7543fc
flejoss <flejoss@gmail.com>
2014-04-22 12:29:25 +0200

    p

--- src/VieilleSardine/CommandeBundle/Controller/CommandeGroupeController.php
+++ src/VieilleSardine/CommandeBundle/Controller/CommandeGroupeController.php
@@ -20,23 +20,46 @@ class CommandeGroupeController extends Controller
 {
      public function indexAction()
     {
-        return $this->render('VieilleSardineCommandeBundle:Commande:IHMCommandeGroupeVPC.html.twig');
+         $session = $request->getSession();
+
+        // stocke un attribut pour une réutilisation lors d'une future requête utilisateur
+      //  $session->set('foo', 'bar');
+
+        // dans un autre contrôleur pour une autre requête
+        $idCmdG = $session->get('idCommande');
+        $tab[] = CreerFormRecapCommandeGroupeAction($idCmdG);
+        
+        $idClt = $session->get('idClient');
+        $form = formClientAction($idClt);
+        // utilise une valeur par défaut si la clé n'existe pas
+      //  $filters = $session->get('filters', array());
+        
+        return $this->render('VieilleSardineCommandeBundle:Commande:IHMCommandeGroupeVPC.html.twig', array('form' => 
+            $form->createView()), array("tab" => $tab[])
+                ); 
     }
     // Méthode qui créer un tableau avec toutes les infos de la commande groupée
-    public function CreerFormRecapCommandeGroupeAction()
+    public function CreerFormRecapCommandeGroupeAction($idCmdSimple)
     {
-        $commandeGroupe = $this->getDoctrine()
-        ->getRepository('CommandeBundle:CommandeGroupee')
-        ->find($id);
+        $listeCmdSimples = $this->getDoctrine()
+        ->getRepository('CommandeBundle:Commande_Groupee')
+        ->find($idCmdSimple);
 
-    if (!$produit) {
-        throw $this->createNotFoundException(
-            'Aucun produit trouvé pour cet id : '.$id
-        );
-    }
+        if (!listeCmdSimple) {
+            throw $this->createNotFoundException(
+                'Aucune commande trouvée pour cet id : '.$id
+            );
+        }
+ 
+        $ligne[] = $this->getDoctrine()
+                ->getRepository('CommandeBundle:Lignes')
+                ->find(listeCmdSimple);
+   //         $listeCmdSimples->find("VehicleCatalogue\Model\Car", array("name" => "Audi A8", "year" => 2010));
+        return $this->render( array("ligne" => $lignes[])
+                ); 
     }
     
-    public function formNewClientAction(Request $request)
+    public function formClientAction(Request $request)
     {
         $client = new Client();
         $compteclient = new CompteClient();
@@ -45,13 +68,12 @@ class CommandeGroupeController extends Controller
         $form = $this->createFormBuilder($client)
             ->add('nom', 'text')
             ->add('prenom', 'text')
-            ->add('mail', 'text')
+            //->add('mail', 'text')
             ->add('codePostal', 'text')
             ->add('ville', 'text')
             ->getForm();
 
-        return $this->render('AcmeTaskBundle:Default:new.html.twig', array(
-            'form' => $form->createView(),
+        return $this->render( array('form' => $form->createView(),
         ));
     }
     
--- src/VieilleSardine/CommandeBundle/Controller/SuiviCommandeController.php
+++ src/VieilleSardine/CommandeBundle/Controller/SuiviCommandeController.php
@@ -6,13 +6,31 @@ namespace VieilleSardine\CommandeBundle\Controller;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SuiviCommandeController extends Controller
 {
      public function indexAction()
     {
-        return $this->render('VieilleSardineCommandeBundle:Commande:IHMSuiviCommande.html.twig');
+         /*
+          *     $manageur = $this->getDoctrine()->getManager();
+    $listecommandes = $manageur->getRepository("CommandeBundle:Commande")->findAll();
+          */
+         // récupérer toutes les commandes & ID & date
+         $requete = Doctrine_Query::create() // On crée une requête.
+		   ->from('commande') // On veut les enregistrements de la table commande.
+		   ->execute(); // On exécute la requête.
+         foreach($requete as $cmd) {
+        // On ajoute la commande dans l'array.
+             $cmds[] = $cmd;
+         }
+        return $this->render('VieilleSardineCommandeBundle:Commande:SuiviCommande.html.twig', array ('cmds'=> $cmds));
     }
     
     
+
+
+
+
 }
