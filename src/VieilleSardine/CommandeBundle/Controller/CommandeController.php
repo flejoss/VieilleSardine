a39bbbb75195343fc79f11e4f7ce8f231a6d8f58
flejoss <flejoss@gmail.com>
2014-04-26 14:37:27 +0200

    modif 26/04
    avancement dans le fichier CommandeController et création de view VCP (
    client et commande ) par guillaume

--- src/VieilleSardine/CommandeBundle/Controller/CommandeController.php
+++ src/VieilleSardine/CommandeBundle/Controller/CommandeController.php
@@ -6,6 +6,7 @@ use VieilleSardine\CommandeBundle\Entity\Lignes;
 use VieilleSardine\ProduitBundle\Entity\Produit;
 use VieilleSardine\UserBundle\Entity\CompteClient;
 use VieilleSardine\UserBundle\Entity\Client;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -68,6 +69,19 @@ class CommandeController extends Controller
             
         
     }
+	    // Méthode pour retrouver les attributs d'un produit
+    public function getInfoProduitAction($id)
+    {
+        $produit = $this->getDoctrine()
+            ->getRepository('ProduitBundle:Produit')
+            ->find($id);
+
+        if (!$produit) {
+            throw $this->createNotFoundException(
+                'Aucun produit trouvé pour cet id : '.$id
+            );
+        }
+    }
     
     // Méthode pour retrouver les attributs d'un produit
     public function tabJsAction()
@@ -103,7 +117,24 @@ class CommandeController extends Controller
 	return $array;
     }
     
-    
+        // Méthode pour l'autocomplétion
+     public function IdProduitAction()
+    {
+    	$request = $this->get('request');
+ 
+    	if($request->isXmlHttpRequest())
+    	{
+    		$term = $request->request->get('idProduit');
+    		$array= $this->getDoctrine()
+    		->getEntityManager()
+    		->getRepository('ProduitBundle:Produit')
+    		->findIdProduitLike($term);
+ 
+    		$response = new Response(json_encode($array));
+    		$response -> headers -> set('Content-Type', 'application/json');
+    		return $response;
+    	}
+    }  
     
      // Méthode qui créer le formulaire utilisé pour ajouter un produit à la commande
     public function CreerFormIDClientAction(Request $request)
@@ -180,6 +211,80 @@ class CommandeController extends Controller
           return $this->render('VieilleSardineCommandeBundle:Commande:IHMRecapCommande.html.twig'
         ); 
     }
+    // Méthode JQuerry pour renseigner toutes les cases du produit en fonction de son id
+    public function InfoProduitAction(Request $request)
+    {
+        $UnClient = new Client();
+        $form = $this->createFormBuilder($UnClient)
+            ->add('nom', 'text')
+            ->add('prenom', 'text')
+            ->add('numeroVoie', 'number')
+            ->add('typeVoie', 'text')
+            ->add('codePostal', 'text')
+            ->add('ville', 'text')
+            ->add('pays', 'text')
+       
+            ->add('Valider', 'submit')
+            ->getForm();
+        
+         $form->handleRequest($request);
+        return $this->render('VieilleSardineCommandeBundle:Commande:IHMClientVPC.html.twig', array('form' => 
+            $form->createView(),
+        )); 
+    }
+
+         public function RecapAllCommandesNomClientAction()
+    {
+        $manageur = $this->getDoctrine()->getManager();
+        $listecommandes = $manageur->getRepository("CommandeBundle:Commande")->findAll();
+         foreach($listecommandes as $cmd) {
+        // On ajoute la commande dans l'array.
+            $em = $this->getDoctrine()->getEntityManager();
+            $client = $em->getRepository('VieilleSardineUserBundle:Client')->findOneByidClient($listecommandes->getIdClient());
+             $tab[] = array(client => $client->getPrenom(), commandeID => $cmd.getIdCommande(), commandeDate => $cmd.getDateCommande() );
+         }
+        return $this->render('VieilleSardineCommandeBundle:Commande:SuiviCommande.html.twig', $tab);
+    }
+    
+    public function detailCommandeAction($idCmd)
+    {
+        $manageur = $this->getDoctrine()->getManager();
+        $commande = $manageur->getRepository("CommandeBundle:Commande")->find($idCmd);
+        $manag = $this->getDoctrine()->getManager();
+        $listeLignes = $manag->getRepository("CommandeBundle:Lignes")->findByidCommande($idCmd);
+        //getEstGroupee()
+        //getPourcentageRemise()
+        //montant
+        foreach($listeLignes as $ligne) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $pdt = $em->getRepository('VieilleSardineProduitBundle:Produit')->find($ligne.getIdLigne());
+            $pT = $pdt.getPrixHt() * $ligne.getQuantite();
+            $tab[] = array( produit => $pdt->getTitre(), quantité => $ligne.getQuantite(), prixUnitaire => $pdt.getPrixHt(), prixTotal => $pT );
+         }
+         if ($commande.getEstGroupee())
+         {
+             //tab[] = prixTotal => "Groupée";
+         }
+         else{
+             
+         }
+         return $this->render('CommandeBundle:Commande:DetailCommande.html.twig', $tab);
+    }
+    
+    public function suiviCommandePersoAction()
+    {
+        $idClt = $session->get('idClient');
+        $manageur = $this->getDoctrine()->getManager();
+        $listecommandes = $manageur->getRepository("CommandeBundle:Commande")->findByIdClient($idClt);
+        foreach($listecommandes as $cmd) {
+            $tab[] = array(idCmd => $cmd->getIdCommande(), dateCmd => $cmd.getEtatCommande(), etatCmd => $cmd.getDateCommande() );
+         }
+         return $this->render('CommandeBundle:Commande:SuiviCommandePerso.html.twig', $tab);
+    }
+
+
+
+}
 
 
 }
